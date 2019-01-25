e1a4d7a03b1108ad1ae10a4ab3545e0fd4c9131f
Andrey Cizov <acizov@gmail.com>
2019-01-25 19:58:23 +0000

    tests done

--- src/bin.rs
+++ src/bin.rs
@@ -1,210 +1,43 @@
-use openssl::asn1::Asn1Time;
-use openssl::error::ErrorStack;
-use openssl::pkey::{PKey, PKeyRef, Private, Public};
-use openssl::rsa::Rsa;
-use openssl::x509::*;
-use openssl::x509::extension::*;
-use openssl::bn::{BigNum, MsbOption};
-use openssl::hash::MessageDigest;
 use clap::App;
 use clap::SubCommand;
 use clap::Arg;
+use simpleca::*;
 use std::fs::OpenOptions;
-use std::io::{Read, Write, Error as IOError};
-use openssl::nid::Nid;
-use openssl::stack::Stack;
-
-fn build_privkey() -> Result<PKey<Private>, ErrorStack> {
-    let rsa = Rsa::generate(2048)?;
-    let privkey = PKey::from_rsa(rsa)?;
-
-    Ok(privkey)
-}
-
-fn build_ca_cert(privkey: &PKey<Private>) -> Result<X509, ErrorStack> {
-    let mut x509_name = X509NameBuilder::new()?;
-    x509_name.append_entry_by_text("C", "US")?;
-    x509_name.append_entry_by_text("ST", "TX")?;
-    x509_name.append_entry_by_text("O", "Some CA organization")?;
-    x509_name.append_entry_by_text("CN", "ca test")?;
-    let x509_name = x509_name.build();
-
-    let mut cert_builder = X509::builder()?;
-    cert_builder.set_version(2)?;
-    let serial_number = {
-        let mut serial = BigNum::new()?;
-        serial.rand(159, MsbOption::MAYBE_ZERO, false)?;
-        serial.to_asn1_integer()?
-    };
-    cert_builder.set_serial_number(&serial_number)?;
-    cert_builder.set_subject_name(&x509_name)?;
-    cert_builder.set_issuer_name(&x509_name)?;
-    cert_builder.set_pubkey(&privkey)?;
-    let not_before = Asn1Time::days_from_now(0)?;
-    cert_builder.set_not_before(&not_before)?;
-    let not_after = Asn1Time::days_from_now(3650)?;
-    cert_builder.set_not_after(&not_after)?;
-
-    cert_builder.append_extension(BasicConstraints::new().critical().ca().build()?)?;
-    cert_builder.append_extension(KeyUsage::new()
-        .critical()
-        .key_cert_sign()
-        .crl_sign()
-        .build()?)?;
-
-    let subject_key_identifier =
-        SubjectKeyIdentifier::new().build(&cert_builder.x509v3_context(None, None))?;
-    cert_builder.append_extension(subject_key_identifier)?;
-
-    cert_builder.sign(&privkey, MessageDigest::sha256())?;
-    let cert = cert_builder.build();
-
-    Ok(cert)
-}
-
-/// Make a X509 request with the given private key
-fn build_ca_req(
-    ca_cert: &X509Ref,
-    privkey: &PKey<Private>,
-    dns: Vec<&str>,
-) -> Result<X509Req, ErrorStack> {
-    let mut req_builder = X509ReqBuilder::new()?;
-    req_builder.set_pubkey(&privkey)?;
-
-    let mut x509_name = X509NameBuilder::new()?;
-    x509_name.append_entry_by_text("C", "US")?;
-    x509_name.append_entry_by_text("ST", "TX")?;
-    x509_name.append_entry_by_text("O", "Some organization")?;
-    x509_name.append_entry_by_text("CN", "www.example.com")?;
-    let x509_name = x509_name.build();
-    req_builder.set_subject_name(&x509_name)?;
-
-    let mut extensions = Stack::<X509Extension>::new()?;
-
-    extensions.push(BasicConstraints::new().build()?)?;
-
-    extensions.push(KeyUsage::new()
-        .critical()
-        .non_repudiation()
-        .digital_signature()
-        .key_encipherment()
-        .build()?)?;
-
-
-    extensions.push(
-        X509Extension::new_nid(None, None, Nid::NETSCAPE_CERT_TYPE, "SSL Server")?
-    )?;
-
-    extensions.push(
-        X509Extension::new_nid(None, None, Nid::NETSCAPE_COMMENT, "Server Certificate")?
-    )?;
-
-    let subject_key_identifier =
-        SubjectKeyIdentifier::new().build(&req_builder.x509v3_context(None))?;
-    extensions.push(subject_key_identifier)?;
-
-    let auth_key_identifier = AuthorityKeyIdentifier::new()
-        .keyid(false)
-        .issuer(false)
-        .build(&req_builder.x509v3_context(None))?;
-    extensions.push(auth_key_identifier)?;
-
-    let mut subject_alt_name = SubjectAlternativeName::new();
-
-    for name in dns {
-        subject_alt_name.dns(name);
-    }
-
-    let subject_alt_name = subject_alt_name.build(&req_builder.x509v3_context(None))?;
-
-    extensions.push(subject_alt_name)?;
-
-    req_builder.add_extensions(&extensions);
-
-    req_builder.sign(&privkey, MessageDigest::sha256())?;
-    let req = req_builder.build();
-    Ok(req)
-}
-
-fn build_ca_signed_cert_server(
-    ca_cert: &X509Ref,
-    ca_privkey: &PKeyRef<Private>,
-    pubkey: &PKey<Public>,
-    req: &X509Req,
-) -> Result<X509, ErrorStack> {
-
-
-    let mut cert_builder = X509::builder()?;
-    cert_builder.set_version(2)?;
-    let serial_number = {
-        let mut serial = BigNum::new()?;
-        serial.rand(159, MsbOption::MAYBE_ZERO, false)?;
-        serial.to_asn1_integer()?
-    };
-
-    //let req = build_ca_req(&ca_cert, &privkey, dns)?;
-
-    cert_builder.set_serial_number(&serial_number)?;
-    cert_builder.set_subject_name(req.subject_name())?;
-    cert_builder.set_issuer_name(ca_cert.subject_name())?;
-    cert_builder.set_pubkey(pubkey)?;
-    let not_before = Asn1Time::days_from_now(0)?;
-    cert_builder.set_not_before(&not_before)?;
-    let not_after = Asn1Time::days_from_now(3650)?;
-    cert_builder.set_not_after(&not_after)?;
-
-    for x in req.extensions()? {
-        cert_builder.append_extension(x);
-    }
-
-    //cert_builder.x509v3_context;
-
-    cert_builder.sign(&ca_privkey, MessageDigest::sha256())?;
-    let cert = cert_builder.build();
-
-    Ok(cert)
-}
-
-#[derive(Debug)]
-pub enum LoadError {
-    IO(IOError),
-    OpenSSL(ErrorStack),
-}
-
-impl From<IOError> for LoadError {
-    fn from(x: IOError) -> Self { LoadError::IO(x) }
-}
-
-impl From<ErrorStack> for LoadError {
-    fn from(x: ErrorStack) -> Self { LoadError::OpenSSL(x)}
-}
-
-fn pkey_from_file(file: &mut Read) -> Result<PKey<Private>, LoadError> {
-    let mut pkey_bytes = Vec::<u8>::with_capacity(2048);
-    file.read_to_end(&mut pkey_bytes)?;
-    let res = PKey::<Private>::private_key_from_pem(pkey_bytes.as_ref())?;
-    Ok(res)
-}
-
-
-fn cert_from_file(file: &mut Read) -> Result<X509, LoadError> {
-    let mut pkey_bytes = Vec::<u8>::with_capacity(2048);
-    file.read_to_end(&mut pkey_bytes)?;
-    let res = X509::from_pem(pkey_bytes.as_ref())?;
-    Ok(res)
-}
 
 fn main() {
     let matches = App::new("Simplistic self-signed CA generator")
         .version("0.1")
         .author("Andrey Cizov <acizov@gmail.com>")
         .subcommand(
-            SubCommand::with_name("pkey")
+            SubCommand::with_name("key")
                 .about("generates a private key in PEM format")
                 .arg(
                     Arg::with_name("output")
                         .required(true)
                         .index(1)
+                )
+                .subcommand(
+                    SubCommand::with_name("gen")
+                    .about("generates a private key in PEM format")
+                    .arg(
+                        Arg::with_name("output")
+                            .required(true)
+                            .index(1)
+                    )
+                )
+                .subcommand(
+                    SubCommand::with_name("pub")
+                        .about("generates a public key from private key in PEM format")
+                        .arg(
+                            Arg::with_name("pkey")
+                                .required(true)
+                                .index(1)
+                        )
+                        .arg(
+                            Arg::with_name("output")
+                                .required(true)
+                                .index(2)
+                        )
                 )
         )
         .subcommand(
@@ -235,7 +68,7 @@ fn main() {
                         .index(2)
                 )
                 .arg(
-                    Arg::with_name("pkey")
+                    Arg::with_name("public_key")
                         .required(true)
                         .index(3)
                 )
@@ -250,6 +83,35 @@ fn main() {
                         .multiple(true)
                 )
         )
+        .subcommand(
+            SubCommand::with_name("server")
+                .about("generates a ca certificate from a given private key in PEM format")
+                .arg(
+                    Arg::with_name("ca_cert")
+                        .required(true)
+                        .index(1)
+                )
+                .arg(
+                    Arg::with_name("ca_pkey")
+                        .required(true)
+                        .index(2)
+                )
+                .arg(
+                    Arg::with_name("public_key")
+                        .required(true)
+                        .index(3)
+                )
+                .arg(
+                    Arg::with_name("csr")
+                        .required(true)
+                        .index(4)
+                )
+                .arg(
+                    Arg::with_name("output")
+                        .required(true)
+                        .index(5)
+                )
+        )
         .get_matches();
 
     let open_read = OpenOptions::new().read(true).clone();
@@ -261,9 +123,7 @@ fn main() {
 
         let pkey = build_privkey().unwrap();
 
-        let serialized = pkey.private_key_to_pem_pkcs8().unwrap();
-
-        file.write(serialized.as_ref()).unwrap();
+        pkey_to_file(&mut file, &pkey).unwrap();
     }
 
     if let Some(matches) = matches.subcommand_matches("ca") {
@@ -274,27 +134,34 @@ fn main() {
 
         let cert = build_ca_cert(&pkey).unwrap();
 
-        open_write.open(file_out).unwrap().write(cert.to_pem().unwrap().as_ref()).unwrap();
+        let mut file = open_write.open(file_out).unwrap();
+
+        cert_to_file(&mut file, &cert).unwrap();
     }
 
     if let Some(matches) = matches.subcommand_matches("server") {
         let file_ca_cert = matches.value_of("ca_cert").unwrap();
         let file_ca_pkey = matches.value_of("ca_pkey").unwrap();
-        let file_pkey = matches.value_of("pkey").unwrap();
+        let file_csr = matches.value_of("csr").unwrap();
+        let file_pkey = matches.value_of("public_key").unwrap();
         let file_out = matches.value_of("output").unwrap();
         let alt_names: Vec<&str> = matches.values_of("alt_names").unwrap().collect();
 
         let ca_cert = cert_from_file(&mut open_read.open(file_ca_cert).unwrap()).unwrap();
         let ca_pkey = pkey_from_file(&mut open_read.open(file_ca_pkey).unwrap()).unwrap();
-        let pkey = pkey_from_file(&mut open_read.open(file_pkey).unwrap()).unwrap();
+        let csr = csr_from_file(&mut open_read.open(file_csr).unwrap()).unwrap();
+        let pkey = pkey_public_from_file(&mut open_read.open(file_pkey).unwrap()).unwrap();
 
         let cert = build_ca_signed_cert_server(
             &ca_cert,
             &ca_pkey,
             &pkey,
+            &csr,
             alt_names
         ).unwrap();
 
-        open_write.open(file_out).unwrap().write(cert.to_pem().unwrap().as_ref()).unwrap();
+        let mut file = open_write.open(file_out).unwrap();
+
+        cert_to_file(&mut file, &cert).unwrap();
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,1 +1,97 @@
 // create tests where SslConnector is created, CA is created and checked against that CA
+
+use openssl::ssl::SslAcceptor;
+use openssl::ssl::SslMethod;
+
+use crate::*;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use openssl::ssl::SslConnector;
+use tempfile::tempdir;
+use std::path::Path;
+use std::fs::OpenOptions;
+use std::io::Write;
+
+fn priv_to_pub(server_key: &PKey<Private>) -> PKey<Public> {
+    return PKey::<Public>::public_key_from_pem(server_key.public_key_to_pem().unwrap().as_ref()).unwrap();
+}
+
+#[test]
+fn test_initialization() {
+    let dir = tempdir().unwrap();
+
+
+    let key = build_privkey().unwrap();
+    let ca = build_ca_cert(&key).unwrap();
+
+    let server_key = build_privkey().unwrap();
+    let csr = build_ca_req(
+        &server_key,
+    ).unwrap();
+
+    let client_key = build_privkey().unwrap();
+    let client_csr = build_ca_req(
+        &client_key,
+    ).unwrap();
+
+    let server_key_pub = priv_to_pub(&server_key);
+    let client_key_pub = priv_to_pub(&client_key);
+
+    let server_cert = build_ca_signed_cert_server(
+        &ca,
+        &key,
+        &server_key_pub,
+        &csr,
+        vec!["localhost"],
+    ).unwrap();
+
+    let client_cert = build_ca_signed_cert_client(
+        &ca,
+        &key,
+        &client_key_pub,
+        &client_csr,
+    ).unwrap();
+
+    let ca_file_name = dir.path().join(Path::new("ca"));
+
+    let ca_file_name = dbg!(ca_file_name);
+
+    OpenOptions::new().write(true).create_new(true).open(&ca_file_name).unwrap().write(&ca.to_pem().unwrap()).unwrap();
+
+    let mut acceptor = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    acceptor.set_certificate(&server_cert).unwrap();
+    acceptor.set_private_key(&server_key).unwrap();
+    acceptor.set_ca_file(&ca_file_name).unwrap();
+    acceptor.check_private_key().unwrap();
+
+    let acceptor = acceptor.build();
+
+    let listener = TcpListener::bind("0.0.0.0:8443").unwrap();
+
+    let mut connector = SslConnector::builder(SslMethod::tls()).unwrap();
+    connector.set_certificate(&client_cert.clone()).unwrap();
+    connector.set_private_key(&client_key).unwrap();
+    connector.set_ca_file(&ca_file_name).unwrap();
+    connector.check_private_key().unwrap();
+    let connector = connector.build();
+
+    thread::spawn(move || {
+        let stream = TcpStream::connect("localhost:8443").unwrap();
+        stream.set_nodelay(true).unwrap();
+        let mut stream = connector.connect("localhost", stream).unwrap();
+
+        let buff = vec![1, 2, 3];
+        stream.write(buff.as_ref()).unwrap();
+    });
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        stream.set_nodelay(true).unwrap();
+        let mut stream = acceptor.accept(stream).unwrap();
+
+        let mut buff = Vec::<u8>::with_capacity(128);
+        let a = stream.read_to_end(&mut buff).unwrap();
+        assert_eq!(a, 3);
+        break;
+    }
+}
